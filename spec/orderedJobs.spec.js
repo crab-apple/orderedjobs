c8e631ae4c15693301224265629f120e8ac382b7
andreu <andreu@nb250.localdomain>
2017-02-27 22:46:36 +0100

    Test that output is a string

--- spec/orderedJobs.spec.js
+++ spec/orderedJobs.spec.js
@@ -10,5 +10,49 @@ describe("orderJobs", function(){
     expect(typeof orderJobs).toEqual("function");
   });
 
+  describe("the output", function(){
+
+    it("is a string", function(){
+      forAllValidInputs(function(input, output){
+        return ((typeof output) === "string");
+      });
+    });
+
+  });
+
+  function forAllValidInputs(predicate){
+    const predicateHolds = allValidInputs().every(function(input){
+      const output = orderJobs(input);
+      return predicate(input, output);
+    });
+    expect(predicateHolds).toBe(true);
+  }
+
+  function allValidInputs(){
+    return [
+
+      "",
+
+      "a =>",
+
+      "a =>\n" +
+      "b =>\n" +
+      "c =>",
+
+      "a =>\n" +
+      "b => c\n" +
+      "c =>",
+
+      "a =>\n" +
+      "b => c\n" +
+      "c => f\n" +
+      "d => a\n" +
+      "e => b\n" +
+      "f =>"
+
+    ]
+  }
+
 });
+
 
